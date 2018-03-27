f8dcda81a4d0aba4790967089efbd3c94b0c08db
nrivkin <noah.rivkin@students.olin.edu>
2018-03-26 20:10:16 -0400

    update blinky for BMS master connections

--- boards/Blinky/Blinky.c
+++ boards/Blinky/Blinky.c
@@ -4,12 +4,12 @@
 int main (void) {
     // Set PB4 to output
     // Use pin 10 to light up an LED
-    DDRB |= _BV(PB5);
+    DDRC |= _BV(PC0);
 
     while(1) {
         // Toggle PE1 (pin 10)
         // Toggles power to pin 10 to create a "blink"
-        PORTB ^= _BV(PB5);
+        PORTC ^= _BV(PC0);
         
 
         // Give a delay to the toggle so it doesn't infinitely toggle
